ab0e28c97ef8922fd3dad790cb8c52e1e28c6b6b
princehabeeb <habiy3400@gmail.com>
2024-05-19 21:47:46 +0100

    new

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React from 'react';
+import IMG1 from '../assets/Rectangle 30.png';
+import '../styles/Works.css'
 
 const Works = () => {
   return (
-    <div>
+    <div className='works'>
       <h2>Featured works</h2>
-      <div></div>
-      <div></div>
-      <div></div>
+      {/* single project */}
+      <div className='project'>
+        <div className="image">
+          <img src={IMG1} alt="dashboard image" />
+        </div>
+        <div className="description">
+          <h1>Ecommerce Dashboard</h1>
+          <span>2020</span> complete
+          <p>
+            Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Iure voluptatem 
+            corporis corrupti, aut fuga distinctio?
+            </p>
+        </div>
+      </div>
+      <div className='project'>
+        <div className="image">
+          <img src={IMG1} alt="dashboard image" />
+        </div>
+        <div className="description">
+          <h1>Ecommerce Dashboard</h1>
+          <span>2020</span> complete
+          <p>
+            Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Iure voluptatem 
+            corporis corrupti, aut fuga distinctio?
+            </p>
+        </div>
+      </div>
+      <div className='project'>
+        <div className="image">
+          <img src={IMG1} alt="dashboard image" />
+        </div>
+        <div className="description">
+          <h1>Ecommerce Dashboard</h1>
+          <span>2020</span> complete
+          <p>
+            Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Iure voluptatem 
+            corporis corrupti, aut fuga distinctio?
+            </p>
+        </div>
+      </div>
     </div>
   )
 }
